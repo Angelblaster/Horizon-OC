d9395d40fa6f2e56ee1f4a6cdde905df07b54b4b
Angelblaster <speedwagon71111@gmail.com>
2026-04-07 16:20:24 +0900

    Update clock frequencies in misc_gui.cpp

--- Source/hoc-clk/overlay/src/ui/gui/misc_gui.cpp
+++ Source/hoc-clk/overlay/src/ui/gui/misc_gui.cpp
@@ -1069,7 +1069,7 @@ protected:
                 NamedValue("2091 MHz", 2091),
                 NamedValue("2193 MHz", 2193),
                 NamedValue("2295 MHz", 2295),
-                NamedValue("2397 MHz", 2397),
+                NamedValue("2397 MHz", 2397),ㄴㄴ
             };
             ValueThresholds eCpuMaxClockThresholds(1785, 2091);
             addConfigButton(
@@ -1250,8 +1250,8 @@ protected:
             NamedValue("2361 MHz", 2361600),
             NamedValue("2380 MHz", 2380800),
             NamedValue("2400 MHz", 2400000, "JEDEC."),
-            NamedValue("2380 MHz", 2500000),
-            NamedValue("2380 MHz", 2600000),
+            NamedValue("2500 MHz", 2500000),
+            NamedValue("2600 MHz", 2600000),
         };
 
         addConfigButtonS(KipConfigValue_eristaEmcMaxClock, "", ValueRange(0, 1, 1, "", 1), "", &eristaRamThresholds, {}, eristaMaxEmcClock, false, A_BTN);
