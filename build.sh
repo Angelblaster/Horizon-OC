ce570b8da748a1b0ea687ab7abc2818055c6079c
Angelblaster <speedwagon71111@gmail.com>
2026-04-08 22:18:09 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -13,12 +13,28 @@ hactool -t kip1 out/nintendo_nx_arm64_armv8a/release/loader.kip --uncompress=hoc
 cd ../../../ # exit
 cp build/stratosphere/loader/hoc.kip dist/atmosphere/kips/hoc.kip
 
-cd Source/hoc-clk/
+cd Source/sys-clk/
 ./build.sh
-cp -r dist/switch/.overlays/horizon-oc-overlay.ovl
+cp -r dist/ ../../
 
 cd ../../
 
 cd Source/Horizon-OC-Monitor/
 make -j"$(nproc)"
 cp Horizon-OC-Monitor.ovl ../../dist/switch/.overlays/Horizon-OC-Monitor.ovl
+
+cd ../../
+
+ROOT="build"
+PATCHES="Atmosphere-Patches"
+
+cp "$PATCHES/secmon_memory_layout.hpp" "$ROOT/libraries/libexosphere/include/exosphere/secmon/"
+cp "$PATCHES/secmon_emc_access_table_data.inc" "$ROOT/exosphere/program/source/smc/"
+cp "$PATCHES/secmon_define_emc_access_table.inc" "$ROOT/exosphere/program/source/smc/"
+cp "$PATCHES/secmon_smc_register_access.cpp" "$ROOT/exosphere/program/source/smc/"
+cd build/exosphere
+
+make -j"$(nproc)"
+
+cd out/nintendo_nx_arm64_armv8a/release
+cp "exosphere.bin" "../../../../../dist/"
